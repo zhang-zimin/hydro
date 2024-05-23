e34e6f1c16715584937702eca400582a00dad326
张子民 <68928662+zhang-zimin@users.noreply.github.com>
2024-05-23 11:04:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -133,17 +133,3 @@ df = pd.DataFrame(random_data, columns=[f'Col{i}' for i in range(1, 11)])
 st.dataframe(df)
 st.table(df)
 
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 10, 100)
-y1 = np.sin(x)
-y2 = np.cos(x)
-
-plt.plot(x, y1, label='sin(x)')
-plt.plot(x, y2, label='cos(x)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-st.pyplot()
