2a0b1ede709d678297c38caf3d5e8b5972d89eb6
张子民 <13542279+github-betterhangitee@user.noreply.gitee.com>
2024-05-23 11:36:48 +0800

    设计哲学

--- app.py
+++ app.py
@@ -35,7 +35,10 @@ st.markdown("[更多水利规范信息](https://example.com/water-resources-spec
 with st.sidebar:
     st.title('欢迎来到水文计算平台')
     st.markdown('---')
-    st.markdown('这是它的特性：\n- feature 1\n- feature 2\n- feature 3')
+    st.markdown('这是streamlit框架的特性：\n- '
+                'Streamlit的设计哲学是简化开发过程，让你专注于应用程序的价值。它追求可访问性，让所有人都能轻松上手使用，无论其技术背景如何。同时，它还追求一致性，提供一个一致的语法和UI'
+                '，以实现高效和直观的工作流程。\n- 你无需编写HTML、CSS或JavaScript，就可以创建复杂的Web应用程序。\n- '
+                'Streamlit提供了多种组件和部件，如按钮、滑块、文本输入框等，这些都可以用于构建交互式应用程序，让用户与应用程序进行互动。')
 
 
 
