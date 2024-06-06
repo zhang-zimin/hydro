16a46c751eee76dfa328cf50673ec3174c2eebd6
张子民 <13542279+github-betterhangitee@user.noreply.gitee.com>
2024-06-06 09:58:51 +0800

    调洪演算df图表展示

--- pages/2_调洪演算.py
+++ pages/2_调洪演算.py
@@ -8,49 +8,47 @@ uploaded_file = st.file_uploader("上传调洪演算输入文件", type=["csv", 
 
 def get_input_data(_file):
     if uploaded_file.name.endswith('.xlsx') or uploaded_file.name.endswith('.xls'):
-        _df = pd.read_excel(uploaded_file)
-    elif _file.name.endswith('.json'):
-        _df = _file
+        dfs = []
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Flood_Hydrograph"))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Reservoir_Capacity"))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Discharge_Curve"))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Outflow"))
+    # elif _file.name.endswith('.json'):
+    #     _df = _file
     else:
         st.error("不支持的文件类型")
-        _df = None
-    return _df
+        dfs = None
+    return dfs
+
+
+def tabs(data):
+    Flood_Hydrograph = data[0]
+    Reservoir_Capacity = data[1]
+    Discharge_Curve = data[2]
+    Outflow = data[3]
+
+    st.title('输入数据展示')
+    tab1, tab2, tab3, tab4 = st.tabs(['洪水过程线', '库容曲线', '下泄曲线', '出库流量'])
+
+    with tab1:
+        st.dataframe(Flood_Hydrograph)
+
+    with tab2:
+        st.dataframe(Reservoir_Capacity)
+
+    with tab3:
+        st.dataframe(Discharge_Curve)
+
+    with tab4:
+        st.dataframe(Outflow)
 
 
 if uploaded_file is not None:
     # st.write(f"你上传的文件是{uploaded_file.name}")
     input_data = get_input_data(uploaded_file)
+    tabs(input_data)
     print(input_data)
     # reservoir = Reservoir(500,input_data)
     # data = reservoir.calculateoutflow()
     # print(data)
     # edited_df = st.data_editor(data)
-
-
-def tabs():
-    st.title('输入数据展示')
-    tab1, tab2, tab3 = st.tabs(['洪水过程线', '下泄曲线', 'imageio'])
-
-    with tab1:
-        '''
-        ```python
-        import cv2
-        image = cv2.imread('image.png')
-        ```
-        '''
-
-    with tab2:
-        '''
-        ```python
-        from PIL import Image
-        image = Image.open('image.png')
-        ```
-        '''
-
-    with tab3:
-        '''
-        ```python
-        import imageio
-        image = imageio.imread('image.png')
-        ```
-        '''
