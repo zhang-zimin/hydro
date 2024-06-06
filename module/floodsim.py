25b1481dd4547fd17ffe9eba8e91cf12b07368c9
张子民 <13542279+github-betterhangitee@user.noreply.gitee.com>
2024-06-06 17:19:23 +0800

    后端算法更新，移除json处理部分，调整类的定义

--- module/floodsim.py
+++ module/floodsim.py
@@ -1,98 +1,89 @@
 # -*- coding: utf-8 -*-
 import math
 import pandas as pd
-import json
+
+
+def a(x, n, d=-99):
+    # x: 任意实数,待转换成n位有效数字的实数
+    # n: 有效数字的位数
+    # d: 小数点后位数，可缺省。
+    x1 = abs(x)
+    n1 = int(math.log10(x1))
+
+    if d == -99:
+        d = max(0, n - n1 - 1)
+
+    x1 = round(x1 * 10 ** d, n - n1 - 1) * 10 ** (n1 - n + 1)
+
+    return x1 if x >= 0 else -x1
+
+
+def zxcz(t0, rg, l):
+    if l not in [1, 2]:
+        return -1
+
+    l2 = 2 if l == 1 else 1
+
+    if t0 < rg[0][l - 1] or t0 > rg[-1][l - 1]:
+        return -1
+
+    for i in range(len(rg) - 1):
+        if rg[i][l - 1] <= t0 <= rg[i + 1][l - 1]:
+            q = rg[i][l2 - 1] + (rg[i + 1][l2 - 1] - rg[i][l2 - 1]) / (rg[i + 1][l - 1] - rg[i][l - 1]) * (
+                    t0 - rg[i][l - 1])
+            break
+    return round(q, 3)
 
 
 class Reservoir:
-
-    def __init__(self,inithead,jsonfile):
-        self.input = self.read_json(jsonfile)  # 接收json输入
-        self.flood_hydrograph = None
-        self.inithead = inithead  #启调水位，用户输入
-        self.reservoir_capacity = None
-        self.discharge_curve = None
-        self.inflow = None
-        self.outflow = None
-
-
-    def zxcz(self, t0, rg, l):
-        if l not in [1, 2]:
-            return -1
-
-        l2 = 2 if l == 1 else 1
-
-        if t0 < rg[0][l - 1] or t0 > rg[-1][l - 1]:
-            return -1
-
-        for i in range(len(rg) - 1):
-            if rg[i][l - 1] <= t0 <= rg[i + 1][l - 1]:
-                q = rg[i][l2 - 1] + (rg[i + 1][l2 - 1] - rg[i][l2 - 1]) / (rg[i + 1][l - 1] - rg[i][l - 1]) * (
-                            t0 - rg[i][l - 1])
-                break
-        return round(q, 3)
-
-    def a(self,x, n, d=-99):
-        # x: 任意实数,待转换成n位有效数字的实数
-        # n: 有效数字的位数
-        # d: 小数点后位数，可缺省。
-        x1 = abs(x)
-        n1 = int(math.log10(x1))
-
-        if d == -99:
-            d = max(0, n - n1 - 1)
-
-        x1 = round(x1 * 10 ** d, n - n1 - 1) * 10 ** (n1 - n + 1)
-
-        return x1 if x >= 0 else -x1
-
-    def read_json(self,jsonfile):
-        with open(jsonfile, 'r') as file:
-            json_data = json.load(file)
-        return json_data
-
-    def save_json(self,data, file_path):
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=2)
+    def __init__(self, inithead, dataframe):
+        self.input = dataframe  # 接收dataframe输入
+        self.inithead = inithead  # 启调水位，用户输入
 
     def calculateoutflow(self):
-        Flood_Hydrograph = pd.DataFrame(self.input["Flood_Hydrograph"])
-        Reservoir_Capacity = pd.DataFrame(self.input["Reservoir_Capacity"])
-        Discharge_Curve = pd.DataFrame(self.input["Discharge_Curve"])
-        q = pd.DataFrame(self.input["Outflow"], columns=["t", "q"])
-
+        Flood_Hydrograph = self.input[0].values
+        Reservoir_Capacity = self.input[1].values
+        Discharge_Curve = self.input[2].values
+        q = pd.DataFrame(self.input[3].values, columns=["t", "q"])
         t = [float(x["t"]) for x in q.to_dict(orient='records')]
         q = [float(x["q"]) for x in q.to_dict(orient='records')]
-
-        Inflow = [self.zxcz(i, Flood_Hydrograph.to_dict(orient='records'), 1) for i in t]
+        Inflow = [zxcz(i, Flood_Hydrograph, 1) for i in t]
         Inflow[-1] = Inflow[0]
-        Reservoir_Capacity_1 = self.zxcz(self.inithead, Reservoir_Capacity.to_dict(orient='records'), 1) * 10000
+        Reservoir_Capacity_1 = zxcz(self.inithead, Reservoir_Capacity, 1) * 10000
 
-        df = pd.DataFrame({'t': t,
-                           'Inflow': Inflow,
-                           'q': q,
-                           'Reservoir_Capacity': Reservoir_Capacity_1,
-                           'Reservoir_Level': self.inithead,
-                           'Calculated_Discharge': 0,
-                           'Discharge_Deviation': 0,
-                           'Discharge': 0})
+        _df = pd.DataFrame({'t': t,
+                            'Inflow': Inflow,
+                            'q': q,
+                            'Reservoir_Capacity': Reservoir_Capacity_1,
+                            'Reservoir_Level': self.inithead,
+                            'Calculated_Discharge': 0,
+                            'Discharge_Deviation': 0,
+                            'Discharge': 0})
 
-        for i in range(1, len(df)):
-            df.iloc[i, 3] = df.iloc[i - 1, 3] + (
-                        df.iloc[i, 1] + df.iloc[i - 1, 1] - df.iloc[i, 2] - df.iloc[i - 1, 2]) / 2 * (
-                                        df.iloc[i, 0] - df.iloc[i - 1, 0]) * 3600
-        for i in range(1, len(df)):
-            df.iloc[i, 4] = self.zxcz(df.iloc[i, 3] / 10000, Reservoir_Capacity.values.tolist(), 2)
-        for i in range(1, len(df)):
-            df.iloc[i, 5] = self.zxcz(df.iloc[i, 4], Discharge_Curve.values.tolist(), 1)
-        for i in range(1, len(df)):
-            df.iloc[i, 6] = df.iloc[i, 2] - df.iloc[i, 5]
-        for i in range(1, len(df)):
-            df.iloc[i, 7] = self.a(df.iloc[i, 2], 3)
-        return df
+        for i in range(1, len(_df)):
+            _df.iloc[i, 3] = _df.iloc[i - 1, 3] + (
+                    _df.iloc[i, 1] + _df.iloc[i - 1, 1] - _df.iloc[i, 2] - _df.iloc[i - 1, 2]) / 2 * (
+                                     _df.iloc[i, 0] - _df.iloc[i - 1, 0]) * 3600
+        for i in range(1, len(_df)):
+            _df.iloc[i, 4] = zxcz(_df.iloc[i, 3] / 10000, Reservoir_Capacity.tolist(), 2)
+        for i in range(1, len(_df)):
+            _df.iloc[i, 5] = zxcz(_df.iloc[i, 4], Discharge_Curve.tolist(), 1)
+        for i in range(1, len(_df)):
+            _df.iloc[i, 6] = _df.iloc[i, 2] - _df.iloc[i, 5]
+        for i in range(1, len(_df)):
+            _df.iloc[i, 7] = a(_df.iloc[i, 2], 3)
+        return _df
 
 
 if __name__ == "__main__":
-    pass
+    dfs = []
+    uploaded_file = r'D:\Devfile\hydro-sty\resources\input.xlsx'
+    dfs.append(pd.read_excel(uploaded_file, sheet_name="Flood_Hydrograph", header=0))
+    dfs.append(pd.read_excel(uploaded_file, sheet_name="Reservoir_Capacity", header=0))
+    dfs.append(pd.read_excel(uploaded_file, sheet_name="Discharge_Curve", header=0))
+    dfs.append(pd.read_excel(uploaded_file, sheet_name="Outflow", header=0))
+    print(dfs[0].values)
+    reservoir = Reservoir(500, dfs)
 
-
+    df = reservoir.calculateoutflow()
+    print(df)
--- pages/2_调洪演算.py
+++ pages/2_调洪演算.py
@@ -2,53 +2,152 @@ from module.floodsim import Reservoir
 import streamlit as st
 import pandas as pd
 import altair as alt
+from pathlib import Path
 
-uploaded_file = st.file_uploader("上传调洪演算输入文件", type=["csv", "xlsx", "json"])
+
+def download():
+    # 读取文件路径
+    file_path = Path('resources/input.xlsx')
+
+    # 读取文件为字节流
+    with open(file_path, 'rb') as f:
+        file_bytes = f.read()
+
+    # 使用download_button并提供字节流
+    st.download_button(
+        label="调洪演算输入文件.xlsx",
+        data=file_bytes,
+        file_name='template.xlsx',
+        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
 
 
 def get_input_data(_file):
     if uploaded_file.name.endswith('.xlsx') or uploaded_file.name.endswith('.xls'):
         dfs = []
-        dfs.append(pd.read_excel(uploaded_file, sheet_name="Flood_Hydrograph"))
-        dfs.append(pd.read_excel(uploaded_file, sheet_name="Reservoir_Capacity"))
-        dfs.append(pd.read_excel(uploaded_file, sheet_name="Discharge_Curve"))
-        dfs.append(pd.read_excel(uploaded_file, sheet_name="Outflow"))
-    # elif _file.name.endswith('.json'):
-    #     _df = _file
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Flood_Hydrograph", header=0))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Reservoir_Capacity", header=0))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Discharge_Curve", header=0))
+        dfs.append(pd.read_excel(uploaded_file, sheet_name="Outflow", header=0))
     else:
         st.error("不支持的文件类型")
         dfs = None
     return dfs
 
 
-def tabs(data):
+
+
+
+def tabshowinputdata(data):
     Flood_Hydrograph = data[0]
     Reservoir_Capacity = data[1]
     Discharge_Curve = data[2]
     Outflow = data[3]
 
-    st.title('输入数据展示')
+    st.header('输入数据展示')
     tab1, tab2, tab3, tab4 = st.tabs(['洪水过程线', '库容曲线', '下泄曲线', '出库流量'])
 
     with tab1:
-        st.dataframe(Flood_Hydrograph)
+        st.subheader('洪水过程线数据')
+        st.dataframe(Flood_Hydrograph, use_container_width=True)
+        st.subheader('设计洪水过程线')
+        st.line_chart(Flood_Hydrograph, x='t(h)', y='Q(m³)')
 
     with tab2:
-        st.dataframe(Reservoir_Capacity)
+        st.subheader('水位库容数据')
+        st.dataframe(Reservoir_Capacity, use_container_width=True)
+        st.subheader('水位库容曲线')
+        st.line_chart(Reservoir_Capacity, x='水位', y='库容(万m³)')
 
     with tab3:
-        st.dataframe(Discharge_Curve)
+        st.subheader('泄洪设施泄流数据')
+        st.dataframe(Discharge_Curve, use_container_width=True)
+        st.subheader('泄洪设施泄流曲线')
+        st.line_chart(Discharge_Curve, x='上游水位(m)', y='总泄量Q(m³/s)')
 
     with tab4:
-        st.dataframe(Outflow)
+        st.subheader('出库流量数据')
+        st.dataframe(Outflow, use_container_width=True)
+        st.subheader('出库流量线')
+        st.line_chart(Outflow, x='t(h)', y='q(m³)')
 
+
+def water_init_level():
+    st.header('计算模块')
+    _level = st.number_input('请您输入起调水位',
+                             value=None)
+    if _level is not None:
+        level = float(_level)
+        # 根据用户输入进行操作
+        if level:
+            st.write('您输入的起调水位是', level)
+        return level
+
+column_mapping = {
+    't': '时间(h)',
+    'Discharge': '削峰后出库洪水过程'
+}
+
+
+def calculate(_water_init_level, _input_data):
+    if st.button('进行计算'):
+        st.subheader("计算结果")
+        reservoir = Reservoir(_water_init_level, _input_data)
+        _result = reservoir.calculateoutflow()
+        _result = _result[['t' , 'Discharge']]
+        # 转为中文
+        _result.rename(columns=column_mapping, inplace=True)
+        # 展示计算结果
+        st.dataframe(_result, use_container_width=True)
+        return _result
+
+
+def explain():
+    st.markdown("""# 水库调洪演算算法""")
+
+
+def plot_a_chart(df):
+    if df is not None:
+        st.subheader("削峰后出库洪水过程")
+        st.line_chart(df, x='时间(h)', y='削峰后出库洪水过程')
+
+        # cols = st.multiselect(
+        #     "选择您想查看的列", list(df.columns), ["时间", "水库库容"]
+        # )
+        # if not cols:
+        #     st.error("至少选择一列")
+        # else:
+        #     print(f"df:{df}")
+        #     data = df.loc[cols]
+        #     chart = (
+        #         alt.Chart(data)
+        #         .mark_area(opacity=0.3)
+        #         .encode(
+        #             x="year:T",
+        #             y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+        #         )
+        #     )
+        #     st.altair_chart(chart, use_container_width=True)
+
+
+explain()
+st.header("上传文件")
+uploaded_file = st.file_uploader("请您点下方选择或者拖拽上传输入文件", type=["xlsx"])
+
+with st.expander("下载输入模板文件"):
+    # 模板文件下载
+    download()
 
 if uploaded_file is not None:
-    # st.write(f"你上传的文件是{uploaded_file.name}")
+    # 获取输入数据
     input_data = get_input_data(uploaded_file)
-    tabs(input_data)
-    print(input_data)
-    # reservoir = Reservoir(500,input_data)
-    # data = reservoir.calculateoutflow()
-    # print(data)
-    # edited_df = st.data_editor(data)
+    # 展示输入的图表
+    tabshowinputdata(input_data)
+    # 获取初始起调整水位
+    water_init_level = water_init_level()
+    # 拿到数据进行计算
+    if water_init_level:
+        # 计算得到结果
+        result = calculate(water_init_level, input_data)
+        # 绘制图表
+        plot_a_chart(result)
